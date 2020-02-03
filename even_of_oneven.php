07ed2664ba29564a986fd806654dc5839e9869a4
daan <2087884@talnet.nl>
2020-02-03 15:30:47 +0100

    even of oneven af

---
+++ even_of_oneven.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo ("noem een getal");
+    $getal = readline(prompt);
+    $getal2 = (2);
+
+    if ($getal % $getal2 == 0){
+        echo ("dit is een even getal");
+    } else {
+        echo ("dit is een oneven getal");
+    }
+
+?>
